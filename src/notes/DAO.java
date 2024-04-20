0b78c2e4ac44c7d54cb1ebb5a769574e090b1059
Ward Quinlan <wardquinlan@outlook.com>
2024-04-20 10:38:28 -0400

    changes

--- src/notes/DAO.java
+++ src/notes/DAO.java
@@ -1,11 +1,28 @@
 package notes;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class DAO {
   public List<Note> search(String filter) throws Exception {
-    return new ArrayList<Note>();
+    List<Note> list = new ArrayList<>();
+    File home = new File(System.getProperty("notes.home"));
+    File[] files = home.listFiles(new FilenameFilter() {
+      @Override
+      public boolean accept(File dir, String name) {
+        return name.endsWith(".txt");
+      }
+    });
+    for (File file: files) {
+      Note note = new Note();
+      note.setTimestamp(new Date(file.lastModified()));
+      note.setTitle(file.getName().substring(0, file.getName().length() - 4));
+      list.add(note);
+    }
+    return list;
   }
   
   public void add(String title, String text) throws Exception {
