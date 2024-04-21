00499fa0782dd5072fbf1b67db59f96dd4dc1d1f
Ward Quinlan <wardquinlan@outlook.com>
2024-04-21 06:30:10 -0400

    changes

--- src/notes/FilterPanel.java
+++ src/notes/FilterPanel.java
@@ -3,6 +3,8 @@ package notes;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -17,6 +19,14 @@ public class FilterPanel extends JPanel {
     super(new FlowLayout(FlowLayout.LEFT));
     
     filter = new JTextField(20);
+    filter.addKeyListener(new KeyAdapter() {
+      @Override
+      public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+          controller.search(filter.getText());
+        }
+      }
+    });
     add(new LabeledComponent("Filter", filter));
     
     JButton search = new JButton("Search");
