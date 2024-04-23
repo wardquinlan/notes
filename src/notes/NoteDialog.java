11d77239c84092f2d1ebaf3ccfd749cedb057839
Ward Quinlan <wardquinlan@outlook.com>
2024-04-23 12:42:55 -0400

    changes

--- src/notes/NoteDialog.java
+++ src/notes/NoteDialog.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -100,6 +102,13 @@ public class NoteDialog extends JDialog {
         if (title != null && !rename) {
           textArea.requestFocus();
         }
+      }
+    });
+    
+    textArea.addKeyListener(new KeyAdapter() {
+      @Override
+      public void keyPressed(KeyEvent e) {
+        setTitle("*Note");
       }
     });
 
