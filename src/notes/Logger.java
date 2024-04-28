693e795a7621b9e394b99e706102420ebd1e6b10
Ward Quinlan <wardquinlan@outlook.com>
2024-04-28 08:39:05 -0400

    Logger class

---
+++ src/notes/Logger.java
@@ -0,0 +1,25 @@
+package notes;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Logger {
+  private static final DateFormat DF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+  private Class<?> clazz;
+  
+  public Logger(Class<?> clazz) {
+    this.clazz = clazz;
+  }
+  
+  public void info(String message) {
+    Date now = Calendar.getInstance().getTime();
+    System.out.println(DF.format(now) + " INFO " + clazz.getName() + " - " + message);
+  }
+  
+  public void error(String message) {
+    Date now = Calendar.getInstance().getTime();
+    System.out.println(DF.format(now) + " ERROR " + clazz.getName() + " - " + message);
+  }
+}
